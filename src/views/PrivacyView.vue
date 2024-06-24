46ee945a19c93b3ed894babf2f543629aa84460b
DevNickjr <nicholasduadei14@gmail.com>
2024-06-24 09:52:09 +0100

    terms and privacy

--- src/views/PrivacyView.vue
+++ src/views/PrivacyView.vue
@@ -1,9 +1,166 @@
 <template>
-    <coming-soon title="Privacy and Policy" heading="Governing Policies" text="In progress"></coming-soon>
+      <x-page-header title="Privacy and Policy"/>
+      <x-container>
+        <div class="flex flex-col pt-10 pb-5 text-center md:text-left md:items-start">
+          <h4 class="font-semibold container-super-title text-linegreen">What information we collect</h4>
+          <div class="flex flex-col gap-3">
+            <p class="text-content">
+              We hold personal data about you that you have given us during your visit to our website including your name and your email address.
+            </p>
+            <p class="text-content">
+              We may collect information about your computer, including your IP address, operating system and browser type, for system administration and in order to create reports.
+            </p>
+            <p class="text-content">
+              This is statistical data about our users’ browsing actions and patterns, and does not identify any individual. The only cookies in use on our site are for Google Analytics. Google Analytics is a web analytics tool that helps website owners understand how visitors engage with their website.
+            </p>
+            <p class="text-content">
+              Google Analytics customers can view a variety of reports about how visitors interact with their website so that they can improve it. Like many services, Google Analytics uses first-party cookies to track visitor interactions as in our case, where they are used to collect information about how visitors use our site. We then use the information to compile reports and to help us improve our site. Cookies contain information that is transferred to your computer’s hard drive.
+            </p>
+            <p class="text-content">
+              These cookies are used to store information, such as the time that the current visit occurred, whether the visitor has been to the site before and what site referred the visitor to the web page. Google Analytics collects information anonymously. It reports website trends without identifying individual visitors. You can opt out of Google Analytics without affecting how you visit our site – for more information on opting out of being tracked by Google Analytics across all websites you use, visit the Google page.
+            </p>
+            <p class="text-content">
+              You may refuse to accept cookies by changing the settings on your browser. However, doing so may affect your ability to access or use certain parts of the Website.  We do not use cookies to collect personally identifiable information about you, track your behaviour or share information with 3rd parties.
+            </p>
+          </div>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 md:text-left md:items-start">
+          <h4 class="font-semibold text-center container-super-title text-linegreen md:text-left">What are your rights?</h4>
+          <p class="text-center text-content md:text-left">
+            Under the regulations you have various legal rights, including the following.
+          </p>
+          <div class="flex flex-col gap-3 my-3">
+            <p class="text-center text-content md:text-left">You have the right to:</p>
+            <ol class="flex flex-col gap-3 pl-12 list-disc text-content">
+              <li>see any data we hold about you free of charge</li>
+              <li>have inaccuracies in any data held about you corrected</li>
+              <li>
+                have information erased (and forgotten by us), subject to retaining commercial and accounts information as described above
+              </li>
+              <li>prevent direct marketing to you by us</li>
+              <li>data portability (to be provided with an electronic copy of the data relevant to you)</li>
+              <li>complain to the regulator (ICO)</li>
+            </ol>
+          </div>
+          <p class="mb-3 text-center text-content md:text-left">You may request to see the data we hold about you, or instruct us on any of these rights you wish to invoke, by getting in touch via any method on the contact page. We will respond within 30 days.</p>
+          <p class="text-center text-content md:text-left">You can also unsubscribe from our mailing list by clicking the ‘unsubscribe’ button at the bottom the newsletters or emails that we send you. This will result in your data being automatically erased from the relevant mailing list.</p>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 text-center md:text-left md:items-start">
+          <h4 class="font-semibold container-super-title text-linegreen">Links to other Websites</h4>
+          <p class="text-content">
+            Our Website may include links to other websites of interest. If you have used these links to leave our site please note we cannot control and is not responsible for the contents or for any privacy or data collection policies they may employ.
+          </p>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 text-center md:text-left md:items-start">
+          <h4 class="font-semibold container-super-title text-linegreen">PRIVACY POLICY</h4>
+          <div class="flex flex-col gap-3">
+            <p class="text-content">
+              This privacy policy sets out how Constellation Global Limited uses and protects any information that you give Constellation Global Limited when you use this website.
+            </p>
+            <p class="text-content">
+              Constellation Global Limited is committed to ensuring that your privacy is protected.  Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.
+            </p>
+            <p class="text-content">
+              Constellation Global Limited may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from 24th June, 2024.
+            </p>
+          </div>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 md:text-left md:items-start">
+          <h4 class="font-semibold text-center md:text-left container-super-title text-linegreen ">What we collect</h4>
+          <div class="flex flex-col gap-3 my-3">
+            <p class="mb-2 text-center md:text-left text-content">We may collect the following information:</p>
+            <ol class="flex flex-col gap-3 pl-12 list-disc text-content">
+              <li>name and job title</li>
+              <li>contact information including email address</li>
+              <li>
+                demographic information such as postcode, preferences and interests
+              </li>
+              <li>other information relevant to customer surveys and/or offers</li>
+            </ol>
+          </div>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 md:text-left md:items-start">
+          <h4 class="font-semibold text-center container-super-title text-linegreen md:text-left">What we do with the information we gather</h4>
+          <div class="flex flex-col gap-3 my-3">
+            <p class="text-center text-content md:text-left">We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:</p>
+            <ol class="flex flex-col gap-3 pl-12 list-disc text-content">
+              <li>Internal record keeping.</li>
+              <li>We may use the information to improve our products and services.</li>
+              <li>
+                We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.
+              </li>
+              <li>From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customize the website according to your interests.</li>
+            </ol>
+          </div>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 text-center md:text-left md:items-start">
+          <h4 class="font-semibold container-super-title text-linegreen">Security</h4>
+          <p class="text-content">
+            We are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.
+          </p>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 text-center md:text-left md:items-start">
+          <h4 class="font-semibold container-super-title text-linegreen">How we use cookies</h4>
+          <div class="flex flex-col gap-3">
+            <p class="text-content">
+              A cookie is a small file which asks permission to be placed on your computer’s hard drive. Once you agree, the file is added and the cookie helps analyse web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences.
+            </p>
+            <p class="text-content">
+              We use traffic log cookies to identify which pages are being used. This helps us analyse data about web page traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.
+            </p>
+            <p class="text-content">
+              Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.
+            </p>
+            <p class="text-content">
+              You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website.
+            </p>
+          </div>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 text-center md:items-start md:text-left">
+          <h4 class="font-semibold container-super-title text-linegreen">Links to other websites</h4>
+          <p class="text-content">
+            Our website may contain links to other websites of interest. However, once you have used these links to leave our site, you should note that we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the website in question.
+          </p>
+        </div>
+      </x-container>
+      <x-container>
+        <div class="flex flex-col pb-5 md:text-left md:items-start">
+          <h4 class="font-semibold text-center container-super-title text-linegreen md:text-left">Controlling your personal information</h4>
+          <div class="flex flex-col gap-3 my-3">
+            <p class="text-center text-content md:text-left">You may choose to restrict the collection or use of your personal information in the following ways:</p>
+            <ol class="flex flex-col gap-3 pl-12 list-disc text-content">
+              <li>whenever you are asked to fill in a form on the website, look for the box that you can click to indicate that you do not want the information to be used by anybody for direct marketing purposes</li>
+              <li>if you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at info@constellation-global.com</li>
+            </ol>
+          </div>
+          <div class="flex flex-col gap-3 text-center md:text-left">
+            <p class="text-content">We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen.</p>
+            <p class="text-content">You may request details of personal information which we hold about you under the Data Protection Act 1998. A small fee will be payable. If you would like a copy of the information held on you please write to Constellation Global Limited, 71-75 Shelton Street, London, WC2H 9JQ.</p>
+            <p class="text-content">If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.</p>
+          </div>
+        </div>
+      </x-container>
 </template>
 <script setup lang="ts">
 import {useHead} from "@vueuse/head";
-import ComingSoon from "@/components/ComingSoon.vue";
+import XPageHeader from "@/components/XPageHeader.vue";
+import XContainer from "@/components/XContainer.vue";
 
 useHead({
   title: 'Privacy and Policy - Constellation Global',
--- src/views/TermsView.vue
+++ src/views/TermsView.vue
@@ -1,18 +1,67 @@
 <template>
-    <coming-soon title="Terms of Service" heading="Binding Conditions" text="In progress"></coming-soon>
+  <x-page-header title="Terms of Use"/>
+  <x-container>
+    <div class="flex flex-col pt-10 pb-5 text-center md:text-left md:items-start">
+      <h4 class="font-semibold container-super-title text-linegreen">Disclaimer</h4>
+      <div class="flex flex-col gap-3">
+        <p class="text-content">
+          <span class="font-semibold">General Information: </span>The information contained in this website is for general information purposes only. The information is provided by Constellation Global Limited and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+        </p>
+        <p class="text-content">
+          <span class="font-semibold">Liability: </span>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+        </p>
+        <p class="text-content">
+          <span class="font-semibold">External Links: </span>Through this website you are able to link to other websites which are not under the control of Constellation Global Limited. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+        </p>
+        <p class="text-content">
+          <span class="font-semibold">Website Availability: </span>Every effort is made to keep the website up and running smoothly. However, Constellation Global Limited takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+        </p>
+      </div>
+    </div>
+  </x-container>
+  <x-container>
+    <div class="flex flex-col pt-10 pb-5 text-center md:text-left md:items-start">
+      <h4 class="font-semibold container-super-title text-linegreen">Usage Terms</h4>
+      <div class="flex flex-col gap-3">
+        <p class="text-content">
+          <span class="font-semibold">Acceptance of Terms: </span>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Constellation Global Limited’s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.
+        </p>
+        <p class="text-content">
+          <span class="font-semibold">Definitions: </span>The term ‘Constellation Global Limited’ or ‘Constellation’ or ‘us’ or ‘we’ refers to Constellation Global Limited and its subsidiary companies whose registered office is 71-75 Shelton Street, London, WC2H 9LH, company registration number is 07530420 registered in England and Wales.  The term ‘you’ refers to the user or viewer of our website.
+        </p>
+        <div class="flex flex-col gap-3 my-3">
+          <p class="text-center text-content md:text-left">The use of this website is subject to the following terms of use:</p>
+          <ol class="flex flex-col gap-3 pl-12 list-disc text-content">
+            <li>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
+            <li>This website uses cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by selected third parties.</li>
+            <li>
+              Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+            </li>
+            <li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.</li>
+            <li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
+            <li>All trademarks reproduced in this website, which are not the property of, or licensed to the operator, are acknowledged on the website.</li>
+            <li>Unauthorised use of this website may give rise to a claim for damages and/or be a criminal offense.</li>
+            <li>From time to time, this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s).</li>
+            <li>Your use of this website and any dispute arising out of such use of the website is subject to the laws of England, Northern Ireland, Scotland and Wales.</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </x-container>
 </template>
 <script setup lang="ts">
 import {useHead} from "@vueuse/head";
-import ComingSoon from "@/components/ComingSoon.vue";
+import XPageHeader from "@/components/XPageHeader.vue";
+import XContainer from "@/components/XContainer.vue";
 
 useHead({
-  title: 'Terms of Service - Constellation Global',
-  meta: [
-    {
-      name: 'og:title',
-      content: 'Terms of Service - Constellation Global',
-    }
-  ]
+title: 'Terms of Use - Constellation Global',
+meta: [
+{
+  name: 'og:title',
+  content: 'Terms of Use - Constellation Global',
+}
+]
 })
 
 
