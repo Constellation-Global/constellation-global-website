ce5d4db179099804f2dd113bcd22670138a13190
DevNickjr <nicholasduadei14@gmail.com>
2024-06-27 21:47:35 +0100

    axios and fetch setup

---
+++ src/composables/useFetch.ts
@@ -0,0 +1,69 @@
+// fetch.js
+import type { AxiosResponse } from 'axios'
+import { ref, type Ref, type UnwrapRef } from 'vue'
+
+interface FetchDataComposable<T> {
+    data: Ref<UnwrapRef<T> | null>;
+    error: Ref<string | null>;
+    loading: Ref<boolean>;
+    fetchData: () => Promise<void>;
+  }
+  
+
+interface IProps<T> {
+    api: (a?: any, b?: any) => Promise<AxiosResponse<T, any>>
+    param?: any
+}
+
+
+export const useFetch = <T,>({ api, param }: IProps<T>): FetchDataComposable<T> => {
+    const data = ref<any>(null);
+
+    const error = ref<string | null>(null);
+    const loading = ref(false);
+
+    const fetchData = async() => {
+        error.value = null;
+        loading.value = true;
+        
+        // const payload = body ? {
+        //     method: "POST",
+        //     headers: {
+        //     "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(body)
+        // } :  {
+        //     method: "GET",
+        //     headers: {
+        //         "authorization": `Bearer ${auth.token}`
+        //     }
+        // };
+
+        try {
+            const response = await api(param);
+            const val = response.data as any
+            
+            if (response.status >= 200 && response.status < 300) {
+                data.value = val;
+            } else {
+                throw new Error("An Error Occured")
+            }
+        } catch (err: any) {
+            console.log("caught", err)
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                error.value = `Error: ${err.response.status} - ${err.response?.data?.message || "An error occured"}`;
+              } else if (err.request) {
+                // The request was made but no response was received
+                error.value = 'Error: No response received';
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                error.value = `Error: ${err.message || 'An error occured'}`;
+              }
+            } finally {
+              loading.value = false;
+            }
+    }
+    return { data, error, loading, fetchData }
+}
