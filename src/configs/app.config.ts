05acc5e53d4bb1e3d7af2b16c34ceb25294859ac
DevNickjr <nicholasduadei14@gmail.com>
2024-07-10 13:30:58 +0100

    .

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
     registerLink: 'https://app.constellation-global.com/employer/auth/register',
-    // baseUrl:  "https://api.constellation-global.com/",
-    baseUrl:  "http://localhost:3200/" || "https://api.constellation-global.com/",
+    baseUrl:  "https://api.constellation-global.com/",
+    // baseUrl:  "http://localhost:3200/" || "https://api.constellation-global.com/",
 }
